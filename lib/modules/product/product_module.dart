318760d85cee63d3214973c35fe684e2952e3652
Ashish Rawat <ashishrawat2911@gmail.com>
2021-10-24 18:10:22 +0530

    code refactor

--- lib/modules/product/product_module.dart
+++ lib/modules/product/product_module.dart
@@ -1,10 +1,13 @@
+import 'package:fluttercommerce/di/di.dart';
 import 'package:fluttercommerce/modules/app/repo/firestore_repository.dart';
 import 'package:fluttercommerce/modules/module.dart';
-import 'package:fluttercommerce/di/di.dart';
+import 'package:fluttercommerce/modules/product/bloc/all_product_cubit.dart';
 
 class ProductModule extends Module {
   @override
   void registerDependencies() {
-    DI.container.registerFactory(() => FirestoreRepository());
+    final firebaseRepo = DI.container<FirestoreRepository>();
+
+    DI.container.registerFactory(() => AllProductCubit(firebaseRepo));
   }
 }
