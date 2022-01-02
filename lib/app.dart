6ac8380ccb3e4a8bdfa065babdb9af68b5e4b191
Ashish Rawat <ashishrawat2911@gmail.com>
2022-01-02 21:28:46 +0530

    Refactor

--- lib/app.dart
+++ lib/app.dart
@@ -13,7 +13,6 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
       theme: AppTheme.appTheme(),
       debugShowCheckedModeBanner: false,
       routerDelegate: NavigationHandler.routerDelegate,
