2ac2cdaa9dada8551fa79c4ae3fe43c7d05f777b
rawatashish2911 <rawatashish2911@gmail.com>
2022-01-03 16:29:06 +0530

    Refactor

---
+++ mobile/test/widget_test.dart
@@ -0,0 +1,13 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:fluttercommerce/di/di.dart';
+import 'package:fluttercommerce/features/init/bloc/splash_bloc.dart';
+import 'package:fluttercommerce/features/init/screen/splash_screen.dart';
+import 'package:fluttercommerce/features/initializer.dart';
+
+void main() {}
