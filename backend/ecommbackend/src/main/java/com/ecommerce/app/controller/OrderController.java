81ddd6ed710aaa7ac304ebec39cf4d92393e7f6a
rawatashish2911 <rawatashish2911@gmail.com>
2022-04-11 22:43:46 +0530

    Removed JWT for now

--- backend/ecommbackend/src/main/java/com/ecommerce/app/controller/OrderController.java
+++ backend/ecommbackend/src/main/java/com/ecommerce/app/controller/OrderController.java
@@ -3,15 +3,14 @@ package com.ecommerce.app.controller;
 
 import com.ecommerce.app.models.entity.OrderMain;
 import com.ecommerce.app.models.entity.ProductInOrder;
+import com.ecommerce.app.models.entity.UserEntity;
 import com.ecommerce.app.service.OrderService;
 import com.ecommerce.app.service.UserService;
-import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
@@ -26,46 +25,43 @@ public class OrderController {
     UserService userService;
 
     @GetMapping("/order")
-    public Page<OrderMain> orderList(@RequestParam(value = "page", defaultValue = "1") Integer page,
-                                     @RequestParam(value = "size", defaultValue = "10") Integer size,
-                                     Authentication authentication) {
+    public Page<OrderMain> orderList(@RequestParam(value = "phone_number") String phoneNumber,
+                                     @RequestParam(value = "page", defaultValue = "1") Integer page,
+                                     @RequestParam(value = "size", defaultValue = "10") Integer size) throws Exception {
         PageRequest request = PageRequest.of(page - 1, size);
         Page<OrderMain> orderPage;
-        if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_CUSTOMER"))) {
-            orderPage = orderService.findByBuyerEmail(authentication.getName(), request);
-        } else {
-            orderPage = orderService.findAll(request);
-        }
+        UserEntity user = userService.getUserByPhoneNumber(phoneNumber);
+        orderPage = orderService.findByBuyerPhone(user.getName(), request);
+//            orderPage = orderService.findAll(request);
         return orderPage;
     }
 
 
     @PatchMapping("/order/cancel/{id}")
-    public ResponseEntity<OrderMain> cancel(@PathVariable("id") Long orderId, Authentication authentication) {
+    public ResponseEntity<OrderMain> cancel(@PathVariable("id") Long orderId) {
         OrderMain orderMain = orderService.findOne(orderId);
-        if (!authentication.getName().equals(orderMain.getBuyerEmail()) && authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_CUSTOMER"))) {
-
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+//        if (!authentication.getName().equals(orderMain.getBuyerEmail()) && authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_CUSTOMER"))) {
+//
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+//        }
         return ResponseEntity.ok(orderService.cancel(orderId));
     }
 
     @PatchMapping("/order/finish/{id}")
-    public ResponseEntity<OrderMain> finish(@PathVariable("id") Long orderId, Authentication authentication) {
-        if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_CUSTOMER"))) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+    public ResponseEntity<OrderMain> finish(@PathVariable("id") Long orderId) {
+//        if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_CUSTOMER"))) {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+//        }
         return ResponseEntity.ok(orderService.finish(orderId));
     }
 
     @GetMapping("/order/{id}")
-    public ResponseEntity show(@PathVariable("id") Long orderId, Authentication authentication) {
-        boolean isCustomer = authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_CUSTOMER"));
+    public ResponseEntity show(@PathVariable("id") Long orderId) {
+//        boolean isCustomer = authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_CUSTOMER"));
         OrderMain orderMain = orderService.findOne(orderId);
-        if (isCustomer && !authentication.getName().equals(orderMain.getBuyerEmail())) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-
+//        if (isCustomer && !authentication.getName().equals(orderMain.getBuyerEmail())) {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+//        }
         Collection<ProductInOrder> items = orderMain.getProducts();
         return ResponseEntity.ok(orderMain);
     }
--- backend/ecommbackend/src/main/java/com/ecommerce/app/security/JWT/JwtEntryPoint.java
+++ backend/ecommbackend/src/main/java/com/ecommerce/app/security/JWT/JwtEntryPoint.java
@@ -1,30 +1,30 @@
-package com.ecommerce.app.security.JWT;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@Component
-public class JwtEntryPoint implements AuthenticationEntryPoint {
-
-
-    private static final Logger logger = LoggerFactory.getLogger(JwtEntryPoint.class);
-
-    // called if authentication failed
-    @Override
-    public void commence(HttpServletRequest request,
-                         HttpServletResponse response,
-                         AuthenticationException e)
-            throws IOException {
-
-        logger.error("Unauthorized error. Message - {}", e.getMessage());
-        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-    }
-}
+//package com.ecommerce.app.security.JWT;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.web.AuthenticationEntryPoint;
+//import org.springframework.stereotype.Component;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@Component
+//public class JwtEntryPoint implements AuthenticationEntryPoint {
+//
+//
+//    private static final Logger logger = LoggerFactory.getLogger(JwtEntryPoint.class);
+//
+//    // called if authentication failed
+//    @Override
+//    public void commence(HttpServletRequest request,
+//                         HttpServletResponse response,
+//                         AuthenticationException e)
+//            throws IOException {
+//
+//        logger.error("Unauthorized error. Message - {}", e.getMessage());
+//        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+//    }
+//}
