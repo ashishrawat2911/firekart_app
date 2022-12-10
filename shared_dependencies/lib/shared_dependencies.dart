53dbd8143d2c6c7a132dd973c5bf5c370319fcc4
rawatashish2911 <rawatashish2911@gmail.com>
2022-12-10 19:11:17 +0530

    added shared_dependencies

---
+++ shared_dependencies/lib/shared_dependencies.dart
@@ -0,0 +1,6 @@
+library shared_dependencies;
+
+export 'package:get_it/get_it.dart';
+export 'package:freezed_annotation/freezed_annotation.dart';
+export 'package:smartstruct/smartstruct.dart';
+export 'package:json_annotation/json_annotation.dart';
