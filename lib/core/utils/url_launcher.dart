52e20b3b5b7c4c363c5a3a394d454c006207b787
Ashish <ashishrawat2911@gmail.com>
2023-05-24 11:00:03 +0530

    added Auhor name in files

--- lib/core/utils/url_launcher.dart
+++ lib/core/utils/url_launcher.dart
@@ -1,3 +1,18 @@
+/*
+ * ----------------------------------------------------------------------------
+ *
+ * This file is part of the Flutter Commerce open-source project, available at:
+ * https://github.com/ashishrawat2911/flutter_commerce
+ *
+ * Created by: Ashish Rawat
+ * ----------------------------------------------------------------------------
+ *
+ * Copyright (c) 2020 Ashish Rawat
+ *
+ * Licensed under the MIT License.
+ *
+ * ----------------------------------------------------------------------------
+ */
 import 'package:url_launcher/url_launcher.dart';
 
 class UrlLauncher {
--- lib/presentation/features/search/view_model/product_search_viewmodel.dart
+++ lib/presentation/features/search/view_model/product_search_viewmodel.dart
@@ -1,3 +1,18 @@
+/*
+ * ----------------------------------------------------------------------------
+ *
+ * This file is part of the Flutter Commerce open-source project, available at:
+ * https://github.com/ashishrawat2911/flutter_commerce
+ *
+ * Created by: Ashish Rawat
+ * ----------------------------------------------------------------------------
+ *
+ * Copyright (c) 2020 Ashish Rawat
+ *
+ * Licensed under the MIT License.
+ *
+ * ----------------------------------------------------------------------------
+ */
 import 'package:fluttercommerce/core/state_manager/view_model.dart';
 import 'package:fluttercommerce/domain/usecases/search_products_use_case.dart';
 import 'package:injectable/injectable.dart' hide Order;
