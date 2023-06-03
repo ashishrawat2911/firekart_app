d5e07860e767a1ac0fdf9ff245ad199627040f02
Ashish <ashishrawat2911@gmail.com>
2023-06-03 18:20:04 +0530

    refactor

---
+++ create_new_screen.sh
@@ -0,0 +1,61 @@
+FILE_NAME="payment" # prefix of file and folder names
+CLASS_NAME="Payment" # prefix of class name
+PARAMETER_NAME="payment" # prefix of parameters
+
+cd lib/presentation/features/ && pwd && mkdir ${FILE_NAME} &&
+cd ${FILE_NAME} &&
+mkdir view_model && cd view_model && touch "${FILE_NAME}_view_model.dart" &&
+echo "import 'package:fluttercommerce/core/state_manager/view_model.dart';
+import 'package:fluttercommerce/presentation/features/${FILE_NAME}/state/${FILE_NAME}_state.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class "${CLASS_NAME}ViewModel" extends ViewModel<"${CLASS_NAME}State"> {
+  "${CLASS_NAME}ViewModel"() : super(const "${CLASS_NAME}State"());
+
+  Future<void> initialize() async {}
+}" >> "${FILE_NAME}_view_model.dart" && cd .. && pwd &&
+
+mkdir state && cd state && touch "${FILE_NAME}_state.dart" &&
+echo "import 'package:freezed_annotation/freezed_annotation.dart';
+part '"${FILE_NAME}_state".freezed.dart';
+
+@Freezed()
+class "${CLASS_NAME}State" with "_\$${CLASS_NAME}State" {
+  const factory "${CLASS_NAME}State"() = "_${CLASS_NAME}State";
+}" >> "${FILE_NAME}_state.dart" && cd .. && pwd &&
+
+# Creating _view.dart files in presentation/view module and adding common view class.
+mkdir view && cd view && touch "${FILE_NAME}_view.dart" &&
+echo "import 'package:flutter/material.dart';
+import 'package:fluttercommerce/presentation/features/${FILE_NAME}/view_model/${FILE_NAME}_view_model.dart';
+import 'package:fluttercommerce/presentation/features/${FILE_NAME}/state/${FILE_NAME}_state.dart';
+import 'package:fluttercommerce/core/state_manager/base_view.dart';
+
+class "${CLASS_NAME}View" extends StatelessWidget {
+  const "${CLASS_NAME}View"({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseView<"${CLASS_NAME}ViewModel", "${CLASS_NAME}State">(
+      onViewModelReady: (viewModel) {
+        viewModel.initialize();
+      },
+      builder: (context, viewModel, state) {
+        return Scaffold(
+          body: SafeArea(
+            child: Container(),
+          ),
+        );
+      },
+    );
+  }
+}
+" >> "${FILE_NAME}_view.dart" && cd ../../../../../ && pwd &&
+
+
+
+cd lib/presentation/features/${FILE_NAME} && git add .
+
+flutter pub run build_runner build --delete-conflicting-outputs
+dart format
