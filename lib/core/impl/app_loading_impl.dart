0045c9b90b18ce9a0523605efa31d0afd287a941
Ashish <ashishrawat2911@gmail.com>
2023-06-03 11:53:40 +0530

    refactor

--- lib/core/impl/app_loading_impl.dart
+++ lib/core/impl/app_loading_impl.dart
@@ -15,39 +15,53 @@
  */
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+
 // ignore: implementation_imports
 import 'package:flutter_easyloading/src/theme.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:fluttercommerce/core/logger/app_logger.dart';
 
-class AppLoaderImpl {
-  TransitionBuilder initBuilder({TransitionBuilder? builder}) {
+class AppLoader {
+  static TransitionBuilder initBuilder({TransitionBuilder? builder}) {
     return EasyLoading.init(builder: builder);
   }
 
-  Future<void> dismissLoader() {
+  static Future<void> dismissLoader() {
     return EasyLoading.dismiss();
   }
 
-  Widget loader() {
+  static Widget loader() {
     return SpinKitCircle(
       // color: AppColors.primaryColor,
       size: EasyLoadingTheme.indicatorSize,
     );
   }
 
-  Future<void> showError(String error) {
-    // TODO: implement showError
-    throw UnimplementedError();
+  static Future<void> showError(String error) {
+    return EasyLoading.showError(
+      error,
+      duration: const Duration(seconds: 3),
+    );
   }
 
-  Future<void> showLoader({String? status}) {
+  static Future<void> showLoader({String? status}) {
     return EasyLoading.show(
       status: status ?? 'Loading....',
       maskType: EasyLoadingMaskType.clear,
     );
   }
 
-  void showToast(String? status) {
-    // TODO: implement showToast
+  static Future<void> showToast(
+    String? status, {
+    EasyLoadingToastPosition? toastPosition,
+  }) async {
+    AppLogger.log('showToast $status');
+    if (status == null) {
+      return;
+    }
+    await EasyLoading.showToast(
+      status,
+      toastPosition: toastPosition ?? EasyLoadingToastPosition.bottom,
+    );
   }
 }
