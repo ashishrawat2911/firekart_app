26eb1e62ffd52f43aa850aa884107ba8e6a6d9f2
Ashish <ashishrawat2911@gmail.com>
2023-09-07 08:17:14 +0530

    ♻️ refactor: Refactor

---
+++ lib/data/source/local/dao/cart_dao.g.dart
@@ -0,0 +1,8 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cart_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$CartDaoMixin on DatabaseAccessor<FirekartDatabase> {
+  $CartTableEntityTable get cartTableEntity => attachedDatabase.cartTableEntity;
+}
