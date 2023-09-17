5e5fd0ec8b5fe398f69628977d0e30c7f70fc261
rawatashish2911 <rawatashish2911@gmail.com>
2023-09-17 18:10:17 +0530

    ♻️ refactor: Refactor

--- run_mobile_script.sh
+++ run_mobile_script.sh
@@ -1,6 +1,4 @@
-melos get
-melos generateAll
-flutter gen-l10n
-flutter test --no-pub --coverage
-flutter build apk
-flutter build ios --release --no-codesign
+ melos get
+ melos generateAll
+ melos localization
+ melos build
