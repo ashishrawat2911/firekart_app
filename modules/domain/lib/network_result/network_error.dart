4cd44599f22bbd67f9710e69c4a83ace31c056bc
Ashish Rawat <ashishrawat2911@gmail.com>
2023-09-25 15:29:11 +0530

    ✅ test: AuthRepository tests

--- modules/domain/lib/network_result/network_error.dart
+++ modules/domain/lib/network_result/network_error.dart
@@ -8,34 +8,4 @@ class NetworkError {
   String toString() {
     return 'NetworkError{errorMessage: $errorMessage, status: $status}';
   }
-
-  factory NetworkError.fromStatusCode(int statusCode) {
-    switch (statusCode) {
-      case 200:
-        return NetworkError('OK', statusCode);
-      case 201:
-        return NetworkError('Created', statusCode);
-      case 204:
-        return NetworkError('No Content', statusCode);
-      case 400:
-        return NetworkError('Bad Request', statusCode);
-      case 401:
-        return NetworkError('Unauthorized', statusCode);
-      case 403:
-        return NetworkError('Forbidden', statusCode);
-      case 404:
-        return NetworkError('Not Found', statusCode);
-      case 405:
-        return NetworkError('Method Not Allowed', statusCode);
-      case 500:
-        return NetworkError('Internal Server Error', statusCode);
-      case 503:
-        return NetworkError('Service Unavailable', statusCode);
-      default:
-        return NetworkError(
-          'Unknown Error : invalid_status_code ',
-          statusCode,
-        );
-    }
-  }
 }
