ee5c923d26bcc1ccc9d2ae62ff3e08f7d6b3b2cc
rawatashish2911 <rawatashish2911@gmail.com>
2023-09-27 16:31:08 +0530

    ✅ test: Added presentation test

---
+++ modules/presentation/test/features/otp_login/view_model/phone_login_viewmodel_test.dart
@@ -0,0 +1,85 @@
+import 'package:core/di/di.dart';
+import 'package:dartz/dartz.dart';
+import 'package:domain/models/login.dart';
+import 'package:domain/models/network.dart';
+import 'package:domain/repository/auth_repository.dart';
+import 'package:domain/usecases/send_otp_usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:notification/push_notification/push_notification_handler.dart';
+import 'package:presentation/features/otp_login/view_model/otp_login_view_model.dart';
+
+import '../view/otp_login_screen_test.mocks.dart';
+
+@GenerateMocks([
+  PushNotificationHandler,
+  AuthRepository,
+])
+void main() {
+  late OtpLoginViewModel viewModel;
+  late MockAuthRepository mockAuthRepository;
+  late MockPushNotificationHandler pushNotificationHandler;
+
+  const String testPhoneNumber = '+1234567890';
+  const String testOtpCode = '1111';
+  const String testErrorMessage = 'Some Error Message';
+
+  setUpAll(() {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    mockAuthRepository = MockAuthRepository();
+    pushNotificationHandler = MockPushNotificationHandler();
+    viewModel = OtpLoginViewModel(
+      SendOTPUseCase(mockAuthRepository),
+      pushNotificationHandler,
+    );
+    getIt.registerFactory(() => viewModel);
+  });
+
+  test('validateButton sets isButtonEnabled correctly', () {
+    viewModel.validateButton('1234');
+    expect(viewModel.state.isButtonEnabled, true);
+
+    viewModel.validateButton('123');
+    expect(viewModel.state.isButtonEnabled, false);
+  });
+
+  test('sendOtp calls authRepository and updates state', () async {
+    when(mockAuthRepository.login('+1234567890')).thenAnswer(
+      (_) async => Right(Login('2222', false)),
+    );
+
+    await viewModel.sendOtp('+1234567890');
+
+    verify(mockAuthRepository.login('+1234567890')).called(1);
+
+    expect(viewModel.state.newUser, false);
+
+  });
+
+  //TODO Need to fix routing first
+  // test('loginWithOtp handles authentication correctly', () async {
+  //   when(mockAuthRepository.verifyOtp(
+  //     '+1234567890',
+  //     '1234',
+  //     name: 'John Doe',
+  //   )).thenAnswer(
+  //     (_) async => Right(EmptyEntity(true, 'success')),
+  //   );
+  //
+  //   await viewModel.loginWithOtp(
+  //     '+1234567890',
+  //     '1234',
+  //     false,
+  //     'John Doe',
+  //   );
+  //
+  //   verify(mockAuthRepository.verifyOtp(
+  //     '+1234567890',
+  //     '1234',
+  //     name: 'John Doe',
+  //   )).called(1);
+  //
+  //   expect(viewModel.state.confirmOtpLoading, false);
+  // });
+}
